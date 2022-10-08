0c2a27ea0b4cf2224c01065e9d38c0ff81a5e6bb
Gamid <dim.u7kin@yandex.ru>
2022-10-08 05:39:12 +0300

    Made it better

--- calculator.py
+++ calculator.py
@@ -1,23 +1,40 @@
 import math
+
 # площадь треугольника
-lst = list(map(int, input("Введите строны треугольника ").split()))
-p = sum(list) / 2
-S_triangle = math.sqrt(p * (p - lst[0]) * (p - lst[1]) * (p - lst[2]))
-print(S_triangle)
+def triangle():
+    lst = list(map(int, input("Введите строны треугольника: ").split()))
+    p = sum(list) / 2
+    S_triangle = math.sqrt(p * (p - lst[0]) * (p - lst[1]) * (p - lst[2]))
+    return S_triangle
 
 # площадь круга
-r = int(input("Введите радиус круга "))
-S_circle = math.pi * r**2
-print(S_circle)
+def circle():
+    r = int(input("Введите радиус круга: "))
+    S_circle = math.pi * r**2
+    return S_circle
 
 # площадь прямоугольника
-a, b = map(int, input("Введите стороны прямоугольника ").split())
-S_rectangle = a * b
-print(S_rectangle)
+def rectangle():
+    a, b = map(int, input("Введите стороны прямоугольника: ").split())
+    S_rectangle = a * b
+    return S_rectangle
 
 # объем конуса
-R, H = map(int, input("Введите радиус основания и высоту конуса ").split())
-V_conus = 1/3 * (math.pi * R**2 * H)
-print(V_conus)
+def conus():
+    R, H = map(int, input("Введите радиус основания и высоту конуса: ").split())
+    V_conus = 1/3 * (math.pi * R**2 * H)
+    return V_conus
+
+figure = input("Площадь/объем какой фигуры нужно поставить(треугольник, круг, прямоугольник, конус): ")
+
+if figure == 'треугольник':
+    print(triangle())
+elif figure == 'круг':
+    print(circle())
+elif figure == 'прямоугольник':
+    print(rectangle())
+elif figure == 'конус':
+    print(conus())
+else: print('error')
 
 
