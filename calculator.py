8471862db80ed210e01b54de300ff323c23d9c29
Gamid <dim.u7kin@yandex.ru>
2022-10-08 05:19:41 +0300

    added rectangle area calculation

--- calculator.py
+++ calculator.py
@@ -1,12 +1,18 @@
 import math
 # площадь треугольника
-a, b, c = map(int, input("Введите строны треугольника ").split())
-p = (a + b + c) / 2
-S_abc = math.sqrt(p * (p - a) * (p - b) * (p - c))
-print(S_abc)
+lst = list(map(int, input("Введите строны треугольника ").split()))
+p = sum(list) / 2
+S_triangle = math.sqrt(p * (p - lst[0]) * (p - lst[1]) * (p - lst[2]))
+print(S_triangle)
 
 # площадь круга
 r = int(input("Введите радиус круга "))
-S_r = math.pi * r**2
-print(S_r)
+S_circle = math.pi * r**2
+print(S_circle)
+
+# площадь прямоугольника
+a, b = map(int, input("Введите стороны прямоугольника ").split())
+S_rectangle = a * b
+print(S_rectangle)
+
 
