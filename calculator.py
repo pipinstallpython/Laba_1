7469cc29b00547eb06f72783087bb3e75070ae9f
Gamid <dim.u7kin@yandex.ru>
2022-10-08 05:13:56 +0300

    Added calculation of the area of the circle

--- calculator.py
+++ calculator.py
@@ -1,7 +1,12 @@
+import math
 # площадь треугольника
-a, b, c = map(int, input().split())
+a, b, c = map(int, input("Введите строны треугольника ").split())
 p = (a + b + c) / 2
-S = (p * (p - a) * (p - b) * (p - c)) ** 0.5
-print(S)
+S_abc = math.sqrt(p * (p - a) * (p - b) * (p - c))
+print(S_abc)
 
+# площадь круга
+r = int(input("Введите радиус круга "))
+S_r = math.pi * r**2
+print(S_r)
 
