c57046c3f86a021f65a6a866d0e27434460042ad
Gamid <dim.u7kin@yandex.ru>
2022-10-22 12:15:34 +0300

    Небольшие изменения

--- calculator.py
+++ calculator.py
@@ -1,40 +1,21 @@
+import turtle
 import math
 
-# площадь треугольника
-def triangle():
-    lst = list(map(int, input("Введите строны треугольника: ").split()))
-    p = sum(list) / 2
-    S_triangle = math.sqrt(p * (p - lst[0]) * (p - lst[1]) * (p - lst[2]))
-    return S_triangle
+def xt(t):
+    return 16 * math.sin(t) ** 3
+def yt(t):
+    return 13 * math.cos(t) - 5 \
+           * math.cos(2 * t) - 2 * \
+           math.cos(3 * t) - math.cos(4 * t)
 
-# площадь круга
-def circle():
-    r = int(input("Введите радиус круга: "))
-    S_circle = math.pi * r**2
-    return S_circle
-
-# площадь прямоугольника
-def rectangle():
-    a, b = map(int, input("Введите стороны прямоугольника: ").split())
-    S_rectangle = a * b
-    return S_rectangle
-
-# объем конуса
-def conus():
-    R, H = map(int, input("Введите радиус основания и высоту конуса: ").split())
-    V_conus = 1/3 * (math.pi * R**2 * H)
-    return V_conus
-
-figure = input("Площадь/объем какой фигуры нужно поставить(треугольник, круг, прямоугольник, конус): ")
-
-if figure == 'треугольник':
-    print(triangle())
-elif figure == 'круг':
-    print(circle())
-elif figure == 'прямоугольник':
-    print(rectangle())
-elif figure == 'конус':
-    print(conus())
-else: print('error')
-
-
+t = turtle.Turtle()
+t.speed(500)
+turtle.colormode(255)
+turtle.Screen().bgcolor(0, 0, 0)
+for i in range(2550):
+    t.goto((xt(i) * 20, yt(i) * 20))
+    t.pencolor((255 - i) % 255, i % 255, (255 + i) // 2 % 255)
+    t.goto(0, 0)
+t.hideturtle()
+turtle.update()
+turtle.mainloop()
